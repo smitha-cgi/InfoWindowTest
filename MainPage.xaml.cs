cc3642a894f38b81389380b879119636f53f1069
Andrew Smith <andrew.e.smith@cgi.com>
2023-02-06 08:59:00 +1100

    Add project files

---
+++ MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+
+namespace MarkerTest;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();        
+    }
+
+    protected override async void OnAppearing()
+    {
+        Pin pin = new()
+        {
+            Label = "Melbourne",
+            Type = PinType.Place,
+            Location = new(-37.84, 144.94),
+            Address = "123 Elizabeth St\nMelbourne, Victoria\n3000"
+        };
+
+        map.Pins.Add(pin);                
+
+        pin.InfoWindowClicked += (s, args) =>
+        {
+            System.Diagnostics.Trace.WriteLine("Pin info clicked");
+        };
+
+        MapSpan span = MapSpan.FromCenterAndRadius(new(-37.02, 144.96), Distance.FromKilometers(250));
+        map.MoveToRegion(span);
+    }
+}
+
